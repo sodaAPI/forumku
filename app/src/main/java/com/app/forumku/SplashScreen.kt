35ddc5dbd1f8b92d8686e4434534df6e20ebdb85
sodapi <rafyrachmansyahh@gmail.com>
2022-08-01 23:03:22 +0700

    V0.2

--- app/src/main/java/com/app/forumku/SplashScreen.kt
+++ app/src/main/java/com/app/forumku/SplashScreen.kt
@@ -1,11 +1,32 @@
 package com.app.forumku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class SplashScreen : AppCompatActivity() {
+
+    val SPLASH_SCREEN = 5000
+
+    private lateinit var topAnim: Animation
+    private lateinit var logoImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_anim)
+        logoImage = findViewById(R.id.logo)
+        logoImage.animation = topAnim
+
+        Handler().postDelayed({
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        },SPLASH_SCREEN.toLong())
     }
 }
--- app/src/main/java/com/app/forumku/WelcomeActivity.kt
+++ app/src/main/java/com/app/forumku/WelcomeActivity.kt
@@ -1,11 +1,38 @@
 package com.app.forumku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var signinbtn: MaterialButton
+    private lateinit var signupbtn: MaterialButton
+    private lateinit var logoImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        signinbtn = findViewById(R.id.signin_btn)
+        signupbtn = findViewById(R.id.signup_btn)
+        logoImage = findViewById(R.id.logo)
+
+        signinbtn.setOnClickListener {
+            Toast.makeText(this,"Sign In",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+        signupbtn.setOnClickListener {
+            Toast.makeText(this,"Sign Up",Toast.LENGTH_SHORT).show()
+//            val intent = Intent(this,SignUpActivity::class.java)
+//            startActivity(intent)
+        }
+
     }
 }
