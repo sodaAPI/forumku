df761e1836c2bb92b6a14ff88a8dee33ebbe5147
sodapi <rafyrachmansyahh@gmail.com>
2022-08-04 20:36:56 +0700

    V0.3

--- app/src/main/java/com/app/forumku/WelcomeActivity.kt
+++ app/src/main/java/com/app/forumku/WelcomeActivity.kt
@@ -1,11 +1,13 @@
 package com.app.forumku
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
 import com.google.android.material.button.MaterialButton
 
 class WelcomeActivity : AppCompatActivity() {
@@ -22,17 +24,36 @@ class WelcomeActivity : AppCompatActivity() {
         signupbtn = findViewById(R.id.signup_btn)
         logoImage = findViewById(R.id.logo)
 
+
         signinbtn.setOnClickListener {
             Toast.makeText(this,"Sign In",Toast.LENGTH_SHORT).show()
             val intent = Intent(this,SignInActivity::class.java)
-            startActivity(intent)
+//            startActivity(intent)
+
+            val pairs: Array<Pair<View, String>?> = arrayOfNulls(1)
+            pairs[0] = Pair<View, String>(signinbtn, "signinTN")
+
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                this, *pairs
+            )
+
+            startActivity(intent, options.toBundle())
         }
 
         signupbtn.setOnClickListener {
             Toast.makeText(this,"Sign Up",Toast.LENGTH_SHORT).show()
-//            val intent = Intent(this,SignUpActivity::class.java)
+            val intent = Intent(this,SignUpActivity::class.java)
 //            startActivity(intent)
-        }
 
+            val pairs: Array<Pair<View, String>?> = arrayOfNulls(1)
+            pairs[0] = Pair<View, String>(signupbtn, "signupTN")
+
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                this, *pairs
+            )
+
+            startActivity(intent, options.toBundle())
+
+        }
     }
 }
